63e6c8f19415c4c3d4e215a7dd68008733e150ac
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-03-27 20:36:04 +0100

    check the name rather than id

--- src/ajax-form.ts
+++ src/ajax-form.ts
@@ -57,7 +57,7 @@ export class AjaxFormElement extends HTMLElement {
 
                 const input = node as HTMLInputElement
 
-                if( input.id.length > 0 ) {
+                if( input.name.length > 0 ) {
                     result[input.id] = input.value
                 }
             } 
@@ -72,43 +72,38 @@ export class AjaxFormElement extends HTMLElement {
     }
 
     public submit( event?:Event ) {
+        if( !this._form ) {
+            return
+        }
+
+        //console.log( event )
         if (event) {
             event.preventDefault();
         }
 
-        console.log( event )
+        const data = this._serializeFormToJson()   
+        //console.dir( data )
 
-        
-        if( this._form ) {
-
-            const data = this._serializeFormToJson()
-            
-            //console.dir( data )
-
-            try  {
-                fetch( this._form.action, {
-                    method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                    //mode: 'cors', // no-cors, *cors, same-origin
-                    //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                    //credentials: 'same-origin', // include, *same-origin, omit
-                    headers: {
+        try  {
+            fetch( this._form.action, {
+                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                //mode: 'cors', // no-cors, *cors, same-origin
+                //cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                //credentials: 'same-origin', // include, *same-origin, omit
+                headers: {
                     'Content-Type': 'application/json'
-                    },
-                    //redirect: 'follow', // manual, *follow, error
-                    referrerPolicy: 'no-referrer', // no-referrer, *client
-                    body: JSON.stringify(data) // body data type must match "Content-Type" header
-                })
-                .then( res =>  this._fire( 'ajax-form.result', res ))
-                .catch( err => this._fire( 'ajax-form.error', err ))
-
-            }
-            catch( err ) {
-                this._fire( 'ajax-form.error', err  );
-            }
+                },
+                //redirect: 'follow', // manual, *follow, error
+                referrerPolicy: 'no-referrer', // no-referrer, *client
+                body: JSON.stringify(data) // body data type must match "Content-Type" header
+            })
+            .then( res =>  this._fire( 'ajax-form.result', res ))
+            .catch( err => this._fire( 'ajax-form.error', err ))
 
         }
-        
-        return false
+        catch( err ) {
+            this._fire( 'ajax-form.error', err  );
+        }
 
     }
 
